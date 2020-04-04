2a68ef6959b4cdac4ca8d8c2464742f78f3d5aa3
danielroa98 <daniel.roa98@gmail.com>
2020-04-04 14:22:03 -0600

    Initial push

---
+++ sources/Ferrocarril.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Problema de los ferrocarriles
+ * 
+ *  Daniel Roa - A01021960
+ *  04 - abril - 2020
+ */ 
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    cout << "Ferrocarril funciona" << endl;
+
+    return 0;
+}
---
+++ sources/TorresHanoi.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Problema de las Torres de Hanoi
+ * 
+ *  Daniel Roa - A01021960
+ *  04 - abril - 2020
+ */
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    cout << "Torres de Hanoi funciona"
+
+    return 0;
+}
